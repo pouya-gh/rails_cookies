e924abb36e059feaae011adddb541de8530e87ab
Pouya Gharib Pour <p.gharibpour@gmail.com>
2015-07-10 10:19:37 +0430

    add methods to examples controller

--- app/controllers/examples_controller.rb
+++ app/controllers/examples_controller.rb
@@ -1,2 +1,14 @@
 class ExamplesController < ApplicationController
+  def index
+
+  end
+
+  def set_name
+    cookies[:name] = params[:name]
+    render :index
+  end
+
+  def test_page
+
+  end
 end
